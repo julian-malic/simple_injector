a687bb6e8355d3a428be19a9a659bea5f11f0207
julian-malic <100823273+julian-malic@users.noreply.github.com>
2022-03-14 20:06:24 +1100

    Update dllmain.cpp

--- payload/dllmain.cpp
+++ payload/dllmain.cpp
@@ -2,9 +2,9 @@
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD ul_reason_for_call, LPVOID reserved) {
     switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-        MessageBox(NULL, L"Hello!", L"Payload", NULL);
-        break;
+        case DLL_PROCESS_ATTACH:
+            MessageBox(NULL, L"Hello!", L"Payload", NULL);
+            break;
     }
     return TRUE;
 }
